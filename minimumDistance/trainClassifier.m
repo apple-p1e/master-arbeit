7f7706dcffb9afdfb80e1b0a7becbf6e3f69f00f
Vladimir Shkoda <im.not.sxs@gmail.com>
2016-04-09 18:51:32 +0200

    Added description.

--- minimumDistance/trainClassifier.m
+++ minimumDistance/trainClassifier.m
@@ -1,4 +1,6 @@
 function [carsM, trucksM] = trainClassifier(X, y)
+% Return midle points (centers) of the classes.
+% Usage: [carsM, trucksM] = trainClassifier(X, y)
     len = length(y);
     carsM = mean(X(1:len/2, :));
     trucksM = mean(X(len/2+1:len, :));

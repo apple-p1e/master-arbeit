06786d14bb886918d5ed48c64e6452b9d93a005c
Vladimir Shkoda <im.not.sxs@gmail.com>
2016-04-11 11:17:15 +0200

    Took out trainClassifier as a function and changed name for running network script.

---
+++ neuralNetwork/trainClassifier.m
@@ -0,0 +1,28 @@
+function [Theta1, Theta2] = trainClassifier(input_layer_size, ...
+                                            hidden_layer_size, num_labels, X, y)
+% Returns weight matrices Theta1 and Theta2.
+% Usage: [Theta1, Theta2] = trainClassifier(input_layer_size, ...
+%                                           hidden_layer_size, num_labels, X, y)
+
+    initial_Theta1 = randInitializeWeights(input_layer_size, hidden_layer_size);
+    initial_Theta2 = randInitializeWeights(hidden_layer_size, num_labels);
+    initial_nn_params = [initial_Theta1(:) ; initial_Theta2(:)];
+    options = optimset('MaxIter', 50);
+    lambda = 1;
+
+    % Create "short hand" for the cost function to be minimized
+    costFunction = @(p) nnCostFunction(p, ...
+                                       input_layer_size, ...
+                                       hidden_layer_size, ...
+                                       num_labels, X, y, lambda);
+    % Now, costFunction is a function that takes in only one argument (the
+    % neural network parameters)
+    [nn_params, cost] = fmincg(costFunction, initial_nn_params, options);
+
+    % Obtain Theta1 and Theta2 back from nn_params
+    Theta1 = reshape(nn_params(1:hidden_layer_size * (input_layer_size + 1)), ...
+                     hidden_layer_size, (input_layer_size + 1));
+
+    Theta2 = reshape(nn_params((1 + (hidden_layer_size * (input_layer_size + 1))):end), ...
+                     num_labels, (hidden_layer_size + 1));
+end

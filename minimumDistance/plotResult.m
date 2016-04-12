6ff80357f2c41b355273f22d14a2a1e1f9094c68
Vladimir Shkoda <im.not.sxs@gmail.com>
2016-04-13 00:31:48 +0200

    Picked up more mathching colors.

--- minimumDistance/plotResult.m
+++ minimumDistance/plotResult.m
@@ -6,8 +6,8 @@ function plotResult(tc, fc, tt, ft, cC, tC)
 % cC == cars center; tC == trucks center
     
     F2 = 2; F3 = 3; F4 = 4;                                  % feature's numbers
-    BLUE = [0.1 0.1 1]; RED = [1 0.3 0.1];                              % colors
-    CYAN = [0.1 0.8 1]; YELLOW = [1 0.9 0.1];
+    COL1 = [0.1 0.1 0.9]; COL2 = [1.0 .05 .05];            % blue and red colors
+    COL3 = [0.1 0.8 0.9]; COL4 = [1.0 0.8 0.1];         % cyan and yellow colors
     PSIZE = 8;                                                  % size of points
 
     figure; hold on;
@@ -19,10 +19,10 @@ function plotResult(tc, fc, tt, ft, cC, tC)
     % plot3(xt(t), yt(t), zt(t));
 
     % display vehicles
-    if size(tc) scatter3(tc(:,F2), tc(:,F3), tc(:,F4), PSIZE, CYAN); end
-    if size(fc) scatter3(fc(:,F2), fc(:,F3), fc(:,F4), PSIZE, BLUE); end
-    if size(tt) scatter3(tt(:,F2), tt(:,F3), tt(:,F4), PSIZE, RED); end
-    if size(ft) scatter3(ft(:,F2), ft(:,F3), ft(:,F4), PSIZE, YELLOW); end
+    if size(tc) scatter3(tc(:,F2), tc(:,F3), tc(:,F4), PSIZE, COL3); end
+    if size(fc) scatter3(fc(:,F2), fc(:,F3), fc(:,F4), PSIZE, COL1); end
+    if size(tt) scatter3(tt(:,F2), tt(:,F3), tt(:,F4), PSIZE, COL4); end
+    if size(ft) scatter3(ft(:,F2), ft(:,F3), ft(:,F4), PSIZE, COL2); end
 
     c = [cC; tC];                                             % classes' centers
     scatter3(c(:,2), c(:,3), c(:,4), 15, 'black', 'x');        % display centers

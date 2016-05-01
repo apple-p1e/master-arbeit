37022378788077ebb760caaf2ec1cf12e03814e1
Vladimir Shkoda <im.not.sxs@gmail.com>
2016-05-01 15:45:14 +0200

    Fixed.

--- @neuralNetwork/sigmoidGradient.m
+++ @neuralNetwork/sigmoidGradient.m
@@ -1,6 +1,7 @@
-function g = sigmoid(c, z)
-% Compute sigmoid function.
-% Usage: g = sigmoid(z)
+function g = sigmoidGradient(c, z)
+% Return the gradient of the sigmoid function evaluated at z.
+% Usage: g = sigmoidGradient(z)
 
     g = 1.0 ./ (1.0 + exp(-z));
+    g = g - g.^2;
 end

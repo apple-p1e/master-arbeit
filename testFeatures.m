8f060dad0a4dc8f5938bfb92f1d83dc80493a093
Vladimir Shkoda <im.not.sxs@gmail.com>
2016-05-03 13:27:01 +0200

    Testing feature sets.

---
+++ testFeatures.m
@@ -0,0 +1,42 @@
+function [accuracy, carspv, truckspv] = testFeatures(files)
+% Run test for different sets of features.
+% Usage: [accuracy, carspv, truckspv] = testFeatures(files)
+% Files must be a cell type array.
+
+    accuracy = [];
+    carspv = [];
+    truckspv = [];
+    
+    for i = 1:length(files)
+        result = testFeatureSet(files{i});
+        accuracy = [accuracy result(:, 1)];
+        carspv = [carspv result(:, 2)];
+        truckspv = [truckspv result(:, 3)];
+    end
+end
+
+function res = testFeatureSet(filename)
+    res = [];
+    
+    load(filename);
+    [X, mu, sigma] = normalizeFeatures(X);
+    
+    mde = minimumDistance(false);
+    mdm = minimumDistance(true);
+    km = kMeans(2);
+    nb = naiveBayes();
+    nn = neuralNetwork(size(X, 2), 5, 2);
+
+    classifiers = {mde, mdm, km, nb, nn};
+
+    for i = 1:length(classifiers)
+        cl = classifiers{i};
+        cl = train(cl, X, y);
+        p = predict(cl, X);
+        [tc, fc, tt, ft] = evaluateResult(X, y, p);
+        acc = (rows(tc) + rows(tt)) / rows([tc; fc; tt; ft]) * 100;
+        ppv = rows(tc) / (rows(tc) + rows(ft)) * 100;
+        npv = rows(tt) / (rows(tt) + rows(fc)) * 100;
+        res = [res; [acc ppv npv]];
+    end
+end

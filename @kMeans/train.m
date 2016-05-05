f30e6a689d7f9ab4bdfd23f01fd4e29031243ec8
Vladimir Shkoda <im.not.sxs@gmail.com>
2016-05-05 20:06:01 +0200

    Unsupervised und supervised learning.

--- @kMeans/train.m
+++ @kMeans/train.m
@@ -1,23 +1,33 @@
 function classifier = train(c, X, y)
-    centroids = [];
-    accuracy = 0;
-    for j = 1:10
+    if nargin == 2
+        % case of unsupervised learning
+        [_, classifier] = runTraining(c, X);
+    else
+        % if we know true labels of training set, we can chose the best result
+        centroids = [];
+        accuracy = 0;
         classifier = c;
-        K = classifier.('K');
-        classifier = initCentroids(classifier, X, K);
-        idx = [];
-        for i = 1:10
-            % For each example in X, assign it to the closest centroid
-            idx = predict(classifier, X);
-            % Given the memberships, compute new centroids
-            classifier = computeCentroids(classifier, X, idx, K);
+        for j = 1:10
+            [idx, cl] = runTraining(c, X);
+            [tc, fc, tt, ft] = evaluateResult(X, y, idx);
+            acc = (rows(tc) + rows(tt)) / rows([tc; fc; tt; ft]);
+            if acc > accuracy
+                accuracy = acc;
+                centroids = cl.('centroids');
+            end
         end
-        [tc, fc, tt, ft] = evaluateResult(X, y, idx);
-        acc = (rows(tc) + rows(tt)) / rows([tc; fc; tt; ft]);
-        if acc > accuracy
-            accuracy = acc;
-            centroids = classifier.('centroids');
-        end
+        classifier.('centroids') = centroids;
     end
-    classifier.('centroids') = centroids;
+end
+
+function [idx, classifier] = runTraining(c, X)
+    classifier = c;
+    K = classifier.('K');
+    classifier = initCentroids(classifier, X, K);
+    for i = 1:10
+        % For each example in X, assign it to the closest centroid
+        idx = predict(classifier, X);
+        % Given the memberships, compute new centroids
+        classifier = computeCentroids(classifier, X, idx, K);
+    end
 end
